521ab3cab07bc8ed2d7198a2ff7b4c384c0d9c34
Fu-Ming <716585+sary357@users.noreply.github.com>
2023-12-06 14:45:04 +0800

    changed port

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 # Start Gunicorn processes
 # port: 8081
-uvicorn --port 8082  main:app --host 0.0.0.0  --reload --log-config conf/logging.conf
+uvicorn --port 8081  main:app --host 0.0.0.0  --reload --log-config conf/logging.conf
